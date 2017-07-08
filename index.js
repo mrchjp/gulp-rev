48ea0758dbd1beb508c26912f890fb15bc5dcf30
Tuan Nguyen Duy <nguyentuan1204@gmail.com>
2017-07-08 22:48:47 +0700

    Update index.js
    Prefix slash for path in manifest

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ plugin.manifest = (pth, opts) => {
 		const revisionedFile = relPath(file.base, file.path);
 		const originalFile = path.join(path.dirname(revisionedFile), path.basename(file.revOrigPath)).replace(/\\/g, '/');
 
-		manifest[originalFile] = revisionedFile;
+		manifest[originalFile.startsWith('/') ? originalFile : '/' + originalFile] = revisionedFile.startsWith('/') ? revisionedFile : '/' + revisionedFile;
 
 		cb();
 	}, function (cb) {
